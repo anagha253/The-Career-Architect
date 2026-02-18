e7b0dead7b6a313cf2c7973cd7c465bc3ba3ed9e
anagha253 <anu2532002@gmail.com>
2026-02-18 23:44:08 +0530

    agent + rag implementation update

--- agents.py
+++ agents.py
@@ -8,56 +8,91 @@ from dotenv import load_dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.prebuilt import ToolNode, tools_condition
-from typing import Annotated, TypedDict
+from typing import Annotated, TypedDict ,List
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START
 from langchain_core.messages import SystemMessage, AIMessage, ToolMessage
 from google import genai
 from google.genai.types import GenerateContentConfig
-from PyPDF2 import PdfReader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pydantic import BaseModel, Field
 from fastapi import FastAPI, HTTPException
-import chromadb
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 load_dotenv()
 llm_key  = os.getenv("LLM_KEY")
 tavily_key = os.getenv("TAVILY_KEY")
 
-client = chromadb.PersistentClient(path="./data/rag/my_ai_memory")
-collection = client.get(name="user_knowledge")
+embedding = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001",api_key=llm_key)
+vector_store = Chroma(
+    collection_name="user_knowledge",
+    embedding_function=embedding,
+    persist_directory="./data/rag/my_ai_memory"
+)
 
 searcher = TavilySearchResults(max_results=3,api_key=tavily_key)
 tools = [searcher]
 
+
+tool_node =ToolNode(tools)
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.6,api_key=llm_key)
+
+class JobOppurtunity(BaseModel):
+    title: str = Field(description="The job title")
+    company: str = Field(description="Name of the company")
+    link: str = Field(description="URL to the job posting")
+    relevance_score: int = Field(description="Score from 1-10 based on user profile")
+
+
+class State(TypedDict):
+    message: Annotated[list, add_messages]
+    location: str
+    job_list: List[JobOppurtunity]
+    interrupt: str
+
+# class JobInfo(BaseModel):
+#     job_name: str = Field(...)
+#     job_link: str = Field(...)
+
+# does Tavily searches
+researcher_model = model.bind_tools(tools)
+def researcher(state :State):
+    print("I am researching for you....")
+    prompt = SystemMessage(content=(
+        "You are a job researcher. Your goal is to find 3 relevant job openings "
+        "based on the user's location and interest. Use Tavily to search. "
+        "Focus on 'Backend Engineer' or 'AI Engineer' roles."
+    ))
+    messages = [prompt] + state["message"]
+    response = researcher_model.invoke(messages)
+    return {"message":[response]}
+'''
 manager = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0,api_key=llm_key)
 
-#researcher <-->  tavily searches
+researcher <-->  tavily searches
 researcher = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0,api_key=llm_key)
 researcher_with_tools = researcher.bind_tools(tools)
 
-#analyser <---> chromaDB to check on user data(CV,projects)
-analyser = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0,api_key=llm_key)
-query = "List all projects and 1 one line for each of what it does, and academic and work experience  in  summarised into 5 points not details missed"
- 
-query_embed = genai.embed_content(
-    model="models/text-embedding-004",
-    content=query,
-    task_type="retrieval_query"
-)['embedding']
-
-results = collection.query(query_embeddings=[query_embed], n_results=1)
-
-retrieved_fact = results['documents'][0][0]
-analyser_prompt = SystemMessage(
-    content= '''
-
-
+analyser <---> chromaDB to check on user data(CV,projects)
+analyser_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0,api_key=llm_key)
 '''
-)
 
+def analyser(state: State):
+    print("Analysing all the info!!!")
+    context = state["message"][-1].content
+    query = "List all projects and 1 one line for each of what it does, and academic and work experience  in  summarised into 5 points not details missed"
 
-res = analyser.invoke()
+    result = vector_store.as_retriever().invoke(query)
+
+    analysis_prompt = f"Based on this resume data: {result}, " \
+                      f"evaluate these jobs: {context}. " \
+                      "Should we draft an email? Output Yes or No."
+
+    response = model.invoke(analysis_prompt)
+
+    return {"message" : [response], "interrupt": "Yes" in response.content}
+
 
 #writer
 writer = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.6,api_key=llm_key)
@@ -67,33 +102,23 @@ reviewer = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.4,api_k
 
 
 
-
-class JobOppurtunity(BaseModel):
-    role_name: str = Field(...)
-    
-
-class State(TypedDict):
-    message: Annotated[list, add_messages]
-    location: str
-    job_list: list
-
-
-
-tool_node =ToolNode(tools)
-
 builder = StateGraph(State)
 
-builder.add_node(manager)
-builder.add_node(researcher)
-builder.add_node(analyser)
+builder.add_node("researcher",researcher)
+builder.add_node("tools", tool_node)
+builder.add_node("analyser",analyser)
 builder.add_node(writer)
 builder.add_node(reviewer)
 
-builder.add_edge(manager,researcher)
+builder.add_edge(START,researcher)
 builder.add_edge(researcher,tool_node)
 builder.add_edge(researcher,analyser)
 builder.add_conditional_edges(
 
+)
+
+app = builder.compile(
+    
 )
 
 
---
+++ utility.py
@@ -0,0 +1,61 @@
+from PyPDF2 import PdfReader
+import chromadb
+from typing import List
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+from dotenv import load_dotenv
+
+'''
+Retrieval of info from resume and project files
+'''
+
+
+
+load_dotenv()
+llm_key = os.getenv("LLM_KEY")
+
+
+
+
+embedding = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001",api_key=llm_key)
+vector_store = Chroma(
+    collection_name="user_knowledge",
+    embedding_function=embedding,
+    persist_directory="./data/rag/my_ai_memory"
+)
+
+def ingest_pdfs_to_chroma(path: str):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=100
+    )
+    
+    all_chunks = []
+    all_metadatas = []
+    
+
+    reader = PdfReader(path)
+    print(f"--- Processing {path} ---")
+        
+        # Extract text from each page
+    for i, page in enumerate(reader.pages):
+        text = page.extract_text()or""
+        chunks = text_splitter.split_text(text)
+            
+        for chunk in chunks:
+            all_chunks.append(chunk)
+            all_metadatas.append({"source": path, "page": i})
+
+
+    vector_store.add_texts(
+        texts=all_chunks,
+        metadatas=all_metadatas
+    )
+
+    vector_store.persist()
+    print(f"Successfully ingested {len(all_chunks)} chunks into ChromaDB.")
+
+# Usage
+ingest_pdfs_to_chroma("./data/user/Resume.pdf")
